2d894013c9c0aeb07de630596194db2e6f77f7ec
maodou <maodou@ecook.cn>
2020-07-29 13:07:28 +0800

    init

---
+++ ios/XCSADBridge.framework/Versions/A/Headers/XCSADBridge.h
@@ -0,0 +1,20 @@
+//
+//  XCSAdBirdge.h
+//  ADSuyiSDKDemo-iOS
+//
+//  Created by kaifa on 2020/7/28.
+//  Copyright © 2020 陈坤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCSADBridge : NSObject
+
+/// 初始化桥接广告
++ (BOOL)xcsBridgeAdSDK;
+
+@end
+
+NS_ASSUME_NONNULL_END
